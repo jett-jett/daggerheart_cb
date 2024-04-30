8a58672ef549e578760f0b001b560f7f8e161fb8
unknown <jett.ptacek@musco.com>
2024-04-29 21:59:38 -0500

    Added some notes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+struct Character {
+    name :      String,
+    class :     String, //BARD DRUID GUARDIAN RANGER ROGUE SERAPH SORCERER WARRIOR WIZARD
+    subclass:   String,
+    community:  String,
+    agility:    i32,
+    strength:   i32,
+    finesse:    i32,
+    instinct:   i32,
+    presence:   i32,
+    domain_deck String[],
+    experiences String[],
+}
+
+
+
+//class
+//subclass
+//heritage
+//experience
+//domains
+//features
